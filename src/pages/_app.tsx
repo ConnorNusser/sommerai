6f16cbeaa3fcf34ab1649b066cb4991548692ffa
Connor Nusser <connornusser@gmail.com>
2023-05-24 17:19:36 -0500

    add changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,10 @@ import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { CacheProvider } from '@chakra-ui/next-js'
 import type { AppProps } from 'next/app'
+import Navbar from '@/components/navbar/navbar'
 'use client'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <CacheProvider><ChakraProvider><Component {...pageProps} /></ChakraProvider></CacheProvider>
+  return <CacheProvider><ChakraProvider><Navbar/><Component {...pageProps} /></ChakraProvider></CacheProvider>
 }
+ß
